8d4f5606de7f42d74e5686ee935a010531b426d4
Anwesh43 <anweshthecool0@gmail.com>
2018-07-02 09:02:49 +0530

    adding LinkedGridBallView class

--- linkedgridballview/src/main/java/com/anwesh/uiprojects/linkedgridballview/LinkedGridBallView.kt
+++ linkedgridballview/src/main/java/com/anwesh/uiprojects/linkedgridballview/LinkedGridBallView.kt
@@ -8,4 +8,22 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.view.View
-import android.view.MotionEvent 
+import android.view.MotionEvent
+
+class LinkedGridBallView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
