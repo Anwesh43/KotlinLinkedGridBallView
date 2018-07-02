34a334d472cc1c02b4ea418e6ec3f0b155525b8a
Anwesh43 <anweshthecool0@gmail.com>
2018-07-02 10:19:50 +0530

    adding onCompletionListener in Activity

--- app/src/main/java/com/anwesh/uiprojects/kotlinlinkedgridballview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/kotlinlinkedgridballview/MainActivity.kt
@@ -3,14 +3,18 @@ package com.anwesh.uiprojects.kotlinlinkedgridballview
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
 import com.anwesh.uiprojects.linkedgridballview.LinkedGridBallView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LinkedGridBallView.create(this)
+        val view : LinkedGridBallView = LinkedGridBallView.create(this)
         fullScreen()
+        view.addOnCompleteListener({Toast.makeText(this, "animation number ${it+1} completed", Toast.LENGTH_SHORT).show()}) {
+            Toast.makeText(this, "animation number ${it+1} has reset", Toast.LENGTH_SHORT).show()
+        }
     }
 }
 
